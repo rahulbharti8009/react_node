d58e2c6c21bcaa17b94e6bada08a2e33bf4d0348
rahul.bharti <rahul.bharti@kreatetechnologies.com>
2024-08-27 17:58:12 +0530

    working on model

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,21 +7,20 @@ function App() {
   const [jokes, setJokes] = useState([])
 
   useEffect(()=> {
-      axios.get('/api/login').then((res)=> {
-        console.log("TAG", res.data)
-        setJokes(res.data)
-      }).catch((error)=>{
-        console.log("error", JSON.stringify(error))
-        setJokes([])
-      })
+      // axios.get('/api/login').then((res)=> {
+      //   console.log("TAG", res.data)
+      //   setJokes(res.data)
+      // }).catch((error)=>{
+      //   console.log("error", JSON.stringify(error))
+      //   setJokes([])
+      // })
     })
 
     return (
     <>
      <h1>Full stack developer </h1>
 
-     <h1>{jokes?.length}</h1>
-     <h1>{jokes?.message}</h1>
+  
      {/* {jokes.map((item, index)=> { <div>
       <h3>{item.name}</h3>
       <p>{item.content}</p>
